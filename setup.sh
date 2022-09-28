6e374c48a62fd9770211b7da2d2c647b63620b92
jlcloudtea <114410941+jlcloudtea@users.noreply.github.com>
2022-09-28 21:28:53 +0930

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,46 +5,53 @@ read -p "Enter ResourceGroupName: " RESOURCEGROUP
 
 read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
-az group create \
+ az group create \
   --location westus3 \
   --name $RESOURCEGROUP
   
-#---------------------------------------------------------#
-# Configure the virtual machine scale set
- 
+  #---------------------------------------------------------#
+  # Configure the virtual machine scale set
+  
  az vmss create \
   --resource-group $RESOURCEGROUP \
   --name webServerScaleSet \
   --image UbuntuLTS \
   --vm-sku Standard_B1ls \
   --upgrade-policy-mode automatic \
-  --custom-data cloud-init.yaml \
   --admin-username $USERNAME \
   --generate-ssh-keys
- 
   
- # Add a health probe to the load balancer
-  az network lb probe create \
-  --lb-name webServerScaleSetLB \
-  --resource-group $RESOURCEGROUP \
-  --name webServerHealth \
-  --port 80 \
-  --protocol Http \
-  --path /
-  
- # Configure the load balancer to route HTTP traffic to the instances in the scale set
-  az network lb rule create \
-  --resource-group $RESOURCEGROUP \
-  --name webServerLoadBalancerRuleWeb \
-  --lb-name webServerScaleSetLB \
-  --probe-name webServerHealth \
-  --backend-pool-name webServerScaleSetLBBEPool \
-  --backend-port 80 \
-  --frontend-ip-name loadBalancerFrontEnd \
-  --frontend-port 80 \
-  --protocol tcp
-  
- # Done
+#Apply the Custom Script Extension  
+az vmss extension set \
+--publisher Microsoft.Azure.Extensions \
+--version 2.0 \
+--name CustomScript \
+--resource-group $RESOURCEGROUP \
+--vmss-name webServerScaleSet \
+--settings @customConfig.json
+
+# Add a health probe to the load balancer
+az network lb probe create \
+--lb-name webServerScaleSetLB \
+--resource-group $RESOURCEGROUP \
+--name webServerHealth \
+--port 80 \
+--protocol Http \
+--path /
+
+# configure the load balancer to route HTTP traffic to the instances in the scale set
+az network lb rule create \
+--resource-group $RESOURCEGROUP \
+--name webServerLoadBalancerRuleWeb \
+--lb-name webServerScaleSetLB \
+--probe-name webServerHealth \
+--backend-pool-name webServerScaleSetLBBEPool \
+--backend-port 80 \
+--frontend-ip-name loadBalancerFrontEnd \
+--frontend-port 80 \
+--protocol tcp
+
+# Done
 echo '-------------------------------------------------------------'
 echo 'VM Setup Script Completed You can start the troubleshooting'
 echo '-------------------------------------------------------------'
