618203f66e2ae13f7e774897b275c1acfdbf26b8
jlcloudtea <114410941+jlcloudtea@users.noreply.github.com>
2022-09-28 22:20:33 +0930

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,30 +5,30 @@ read -p "Enter ResourceGroupName: " RESOURCEGROUP
 
 read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
- az group create \
-  --location westus3 \
-  --name $RESOURCEGROUP
+az group create \
+ --location westus3 \
+ --name $RESOURCEGROUP
   
-  #---------------------------------------------------------#
-  # Configure the virtual machine scale set
+#---------------------------------------------------------#
+# Configure the virtual machine scale set
   
- az vmss create \
-  --resource-group $RESOURCEGROUP \
-  --name webServerScaleSet \
-  --image UbuntuLTS \
-  --vm-sku Standard_B1ls \
-  --upgrade-policy-mode automatic \
-  --admin-username $USERNAME \
-  --generate-ssh-keys
+az vmss create \
+ --resource-group $RESOURCEGROUP \
+ --name webServerScaleSet \
+ --image UbuntuLTS \
+ --vm-sku Standard_B1ls \
+ --upgrade-policy-mode automatic \
+ --admin-username $USERNAME \
+ --generate-ssh-keys
   
 #Apply the Custom Script Extension  
 az vmss extension set \
---publisher Microsoft.Azure.Extensions \
---version 2.0 \
---name CustomScript \
---resource-group $RESOURCEGROUP \
---vmss-name webServerScaleSet \
---settings @customConfig.json
+ --publisher Microsoft.Azure.Extensions \
+ --version 2.0 \
+ --name CustomScript \
+ --resource-group $RESOURCEGROUP \
+ --vmss-name webServerScaleSet \
+ --settings @customConfig.json
 
 # Add a health probe to the load balancer
 az network lb probe create \
@@ -41,15 +41,41 @@ az network lb probe create \
 
 # configure the load balancer to route HTTP traffic to the instances in the scale set
 az network lb rule create \
---resource-group $RESOURCEGROUP \
---name webServerLoadBalancerRuleWeb \
---lb-name webServerScaleSetLB \
---probe-name webServerHealth \
---backend-pool-name webServerScaleSetLBBEPool \
---backend-port 80 \
---frontend-ip-name loadBalancerFrontEnd \
---frontend-port 80 \
---protocol tcp
+ --resource-group $RESOURCEGROUP \
+ --name webServerLoadBalancerRuleWeb \
+ --lb-name webServerScaleSetLB \
+ --probe-name webServerHealth \
+ --backend-pool-name webServerScaleSetLBBEPool \
+ --backend-port 80 \
+ --frontend-ip-name loadBalancerFrontEnd \
+ --frontend-port 80 \
+ --protocol tcp
+
+# Setup "Environment"
+
+az network nsg create \
+  --resource-group $RESOURCEGROUP \
+  --name vmsetnsg
+
+az network nsg rule create \
+  --resource-group $RESOURCEGROUP \
+  --name vmsetnsgrule \
+  --nsg-name vmsetnsg \
+  --protocol tcp \
+  --direction inbound \
+  --source-address-prefix '*' \
+  --source-port-range '*' \
+  --destination-address-prefix 'VirtualNetwork' \
+  --destination-port-range '*' \
+  --access deny \
+  --priority 110
+  
+#update the existing subnet
+az network vnet subnet update \
+  --resource-group $RESOURCEGROUP \
+  --vnet-name webServerScaleSetVNET \
+  --name webServerScaleSetSubnet \
+  --network-security-group vmsetnsg
 
 # Done
 echo '-------------------------------------------------------------'
