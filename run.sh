6dbb22121255b87e75d7fc8dcc51fd91ce2f8aa8
jlcloudtea <114410941+jlcloudtea@users.noreply.github.com>
2022-09-28 22:52:19 +0930

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
 bash setup.sh >/dev/null 2>&1
+echo '-------------------------------------------------------------'
+echo 'VM Setup Script Completed You can start the troubleshooting'
+echo '-------------------------------------------------------------'
