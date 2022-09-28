fea34ecbbe1d057734922503c991dff672c333e5
jlcloudtea <114410941+jlcloudtea@users.noreply.github.com>
2022-09-29 08:31:44 +0930

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
+echo '-------------------------------------------------------------'
+echo '     Please wait 2-5 mins until new prompt message           '
+echo '-------------------------------------------------------------'
+
 bash setup.sh >/dev/null 2>&1
 echo '-------------------------------------------------------------'
 echo '     Setup Completed You can start the troubleshooting       '
