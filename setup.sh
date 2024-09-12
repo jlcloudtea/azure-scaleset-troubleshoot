6dcca100d0a6de65d62cb54320a2e21ab2b8b7ea
jlcloudtea <114410941+jlcloudtea@users.noreply.github.com>
2024-09-12 23:06:24 +0930

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,18 @@
+#!/bin/bash
 az group create \
  --location eastus \
  --name troubleshoot
-  
+
 #---------------------------------------------------------#
 # Configure the virtual machine scale set
   
 az vmss create \
- --resource-group troubleshoot \
- --name webServerScaleSet \
- --image Ubuntu2204 \
- --vm-sku Standard_B1ls \
- --upgrade-policy-mode automatic \
- --admin-username azureuser \
- --generate-ssh-keys
+  --resource-group troubleshoot \
+  --name TRScaleSet \
+  --image Ubuntu2204 \
+  --orchestration-mode Flexible \
+  --admin-username azureuser \
+  --generate-ssh-keys
   
 #Apply the Custom Script Extension  
 az vmss extension set \
@@ -20,32 +20,33 @@ az vmss extension set \
  --version 2.0 \
  --name CustomScript \
  --resource-group troubleshoot \
- --vmss-name webServerScaleSet \
+ --vmss-name TRScaleSet \
  --settings @customConfig.json
 
-# Add a health probe to the load balancer
-az network lb probe create \
---lb-name webServerScaleSetLB \
---resource-group troubleshoot \
---name webServerHealth \
---port 80 \
---protocol Http \
---path /
-
-# configure the load balancer to route HTTP traffic to the instances in the scale set
-az network lb rule create \
+#Apply the extension to the existing scale set instances
+az vmss update-instances \
  --resource-group troubleshoot \
- --name webServerLoadBalancerRuleWeb \
- --lb-name webServerScaleSetLB \
- --probe-name webServerHealth \
- --backend-pool-name webServerScaleSetLBBEPool \
- --backend-port 80 \
- --frontend-ip-name loadBalancerFrontEnd \
- --frontend-port 80 \
- --protocol tcp
+ --name TRScaleSet \
+ --instance-ids "*"
+
+#Allow traffic to port 80
+az network nsg rule create \
+ --name AllowHTTP \
+ --resource-group troubleshoot \
+ --nsg-name TRScaleSetNSG \
+ --access Allow \
+ --priority 1010 \
+ --destination-port-ranges 80
+
+#Test your scale set
+echo 'PublicIP of LoadBalance is'
+az network public-ip show \
+--resource-group troubleshoot \
+--name TRScaleSetLBPublicIP \
+--query [ipAddress] \
+--output tsv
 
 # Setup "Environment"
-
 az network nsg create \
   --resource-group troubleshoot \
   --name vmsetnsg
@@ -66,13 +67,13 @@ az network nsg rule create \
 #update the existing subnet
 az network vnet subnet update \
   --resource-group troubleshoot \
-  --vnet-name webServerScaleSetVNET \
-  --name webServerScaleSetSubnet \
+  --vnet-name TRScaleSetVNET \
+  --name TRScaleSetSubnet \
   --network-security-group vmsetnsg
   
 #update the existing vm
-InstanceId=`az vmss list-instances -g troubleshoot -n webServerScaleSet --query '[0].instanceId' --output tsv`
-az vmss stop --resource-group troubleshoot --name webServerScaleSet --instance-ids $InstanceId
+InstanceId=`az vmss list-instances -g troubleshoot -n TRScaleSet --query '[0].instanceId' --output tsv`
+az vmss stop --resource-group troubleshoot --name TRScaleSet --instance-ids $InstanceId
 
 # Done
 echo '--------------------------------------------------------------------'
